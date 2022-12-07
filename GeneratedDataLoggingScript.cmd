f62b1d10de8d4c3c5c9d5d7e7653e7a0f92166a7
oashmoore <olivia@energyinnovation.org>
2022-12-06 16:20:32 -0800

    updated rmi files and update to 3.4.3

--- GeneratedDataLoggingScript.cmd
+++ GeneratedDataLoggingScript.cmd
@@ -4,7 +4,7 @@ SIMULATE>SAVELIST|OutputVarsToExport-Calibration2.lst
 SIMULATE>RUNNAME|NoSettings
 SIMULATE>READCIN|
 MENU>RUN|O
-MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport-Calibration2.lst|+!||2019|2050|:NoSettings
+MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport-Calibration2.lst|+!||2020|2050|:NoSettings
 FILE>DELETE|NoSettings.vdfx
 
 SIMULATE>SAVELIST|
